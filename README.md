cc8df5b7ea4887b97ac1b8c918bc852b8da898e7
shubhamgupta04 <subham.g.1995@gmail.com>
2022-07-19 00:53:50 +0530

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # mushroom_classifer
+
+## Git and Github
+
+# To clone git to vscode
+git clone <url>
+dir or ls #check #if it is cloned
+cd <dir> #go to the dir
+code . #open vscode
+
+# To Add files to git
+'''
+git add .
+'''
+OR
+'''
+git add <file_name>
+'''
+
+# To create version/commit all changes by git
+'''
+git commit -m "message"
+'''
+
+# To send version/changes to github
+'''
+git push origin main
+'''
+
+# To pull version/changes from github
+'''
+git pull origin main
+'''
+
+# To check remote url
+'''
+git remote -v
+'''
+
+# To ignore file or folder from git we can write name of file/folder in file
+'''
+.gitignore
+'''
+
+# To check the git status
+'''
+git status
+'''
+
+# To check all version maintained by git
+'''
+git log
+'''
---
+++ app.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+import pickle
+from flask import Flask, request
+import numpy as np
+
+app = Flask(__name__)
+
+classifier_model = pickle.load(open("mushrooms.pkl","rb"))
+
+# http://localhost:5000/mushrooms_predict
+
+@app.route("/mushrooms_predict" , methods = ["GET","POST"])
+def api_predict():
+        if request.method == "GET":
+                return "Please send a post request"
+        elif request.method == "POST":
+        
+                data = request.get_json()
+                
+                capshape = data['cap-shape']
+                capsurface = data['cap-surface']
+                capcolor = data['cap-color']
+                bruises = data['bruises']
+                odor = data['odor']
+                gillattachment = data['gill-attachment']
+                gillspacing = data['gill-spacing']
+                gillsize = data['gill-size']
+                gillcolor = data['gill-color']
+                stalkshape = data['stalk-shape']
+                stalkroot = data['stalk-root']
+                stalk_surface_above_ring = data['stalk-surface-above-ring']
+                stalk_surface_below_ring = data['stalk-surface-below-ring']
+                stalk_color_above_ring = data['stalk-color-above-ring']
+                stalk_color_below_ring = data['stalk-color-below-ring']
+                veilcolor = data['veil-color']
+                ringnumber = data['ring-number']
+                ringtype = data['ring-type']
+                spore_print_color = data['spore-print-color']
+                population = data['population']
+                habitat = data['habitat']
+        
+        
+                input1 = np.array([[capshape,capsurface,capcolor,bruises,odor,gillattachment,gillspacing,gillsize,gillcolor,stalkshape,stalkroot,
+                                stalk_surface_above_ring,stalk_surface_below_ring,stalk_color_above_ring,stalk_color_below_ring,veilcolor,
+                                ringnumber,ringtype,spore_print_color,population,habitat]])
+        
+                prediction = classifier_model.predict(input1)
+                return str(prediction)
+
+app.run()
+
+
+
+import requests
+url = "http://localhost:5000/mushrooms_predict"
+data = {
+    'cap-shape' : 4,
+    'cap-surface': 3,
+    'cap-color': 8,
+    'bruises': 1,
+    'odor': 0,
+    'gill-attachment': 1,
+    'gill-spacing': 0,
+    'gill-size': 1,
+    'gill-color': 7,
+    'stalk-shape': 1,
+    'stalk-root': 3,
+    'stalk-surface-above-ring': 3,
+    'stalk-surface-below-ring': 3,
+    'stalk-color-above-ring': 3,
+    'stalk-color-below-ring': 3,
+    'veil-color': 0,
+    'ring-number': 1,
+    'ring-type': 4,
+    'spore-print-color': 7,
+    'population': 4,
+    'habitat': 6
+
+}
+
+r=requests.post(url, json=data)
+print(r)
+print(r.text)
