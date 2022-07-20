16e12f2c57f748c82f208f9f5b88c04174393b9b
shubhamgupta04 <subham.g.1995@gmail.com>
2022-07-20 15:58:21 +0530

    change in app.py

--- app.py
+++ app.py
@@ -9,43 +9,41 @@ classifier_model = pickle.load(open("mushrooms.pkl","rb"))
 
 # http://localhost:5000/mushrooms_predict
 
-@app.route("/mushrooms_predict" , methods = ["GET","POST"])
+@app.route("/mushrooms_predict" , methods = ['GET','POST'])
+def index():
+    return "CI CD pipeline has been established."
 def api_predict():
-        if request.method == "GET":
-                return "Please send a post request"
-        elif request.method == "POST":
-        
-                data = request.get_json()
+        data = request.get_json()
                 
-                capshape = data['cap-shape']
-                capsurface = data['cap-surface']
-                capcolor = data['cap-color']
-                bruises = data['bruises']
-                odor = data['odor']
-                gillattachment = data['gill-attachment']
-                gillspacing = data['gill-spacing']
-                gillsize = data['gill-size']
-                gillcolor = data['gill-color']
-                stalkshape = data['stalk-shape']
-                stalkroot = data['stalk-root']
-                stalk_surface_above_ring = data['stalk-surface-above-ring']
-                stalk_surface_below_ring = data['stalk-surface-below-ring']
-                stalk_color_above_ring = data['stalk-color-above-ring']
-                stalk_color_below_ring = data['stalk-color-below-ring']
-                veilcolor = data['veil-color']
-                ringnumber = data['ring-number']
-                ringtype = data['ring-type']
-                spore_print_color = data['spore-print-color']
-                population = data['population']
-                habitat = data['habitat']
-        
-        
-                input1 = np.array([[capshape,capsurface,capcolor,bruises,odor,gillattachment,gillspacing,gillsize,gillcolor,stalkshape,stalkroot,
-                                stalk_surface_above_ring,stalk_surface_below_ring,stalk_color_above_ring,stalk_color_below_ring,veilcolor,
-                                ringnumber,ringtype,spore_print_color,population,habitat]])
-        
-                prediction = classifier_model.predict(input1)
-                return str(prediction)
+        capshape = data['cap-shape']
+        capsurface = data['cap-surface']
+        capcolor = data['cap-color']
+        bruises = data['bruises']
+        odor = data['odor']
+        gillattachment = data['gill-attachment']
+        gillspacing = data['gill-spacing']
+        gillsize = data['gill-size']
+        gillcolor = data['gill-color']
+        stalkshape = data['stalk-shape']
+        stalkroot = data['stalk-root']
+        stalk_surface_above_ring = data['stalk-surface-above-ring']
+        stalk_surface_below_ring = data['stalk-surface-below-ring']
+        stalk_color_above_ring = data['stalk-color-above-ring']
+        stalk_color_below_ring = data['stalk-color-below-ring']
+        veilcolor = data['veil-color']
+        ringnumber = data['ring-number']
+        ringtype = data['ring-type']
+        spore_print_color = data['spore-print-color']
+        population = data['population']
+        habitat = data['habitat']
+
+
+        input1 = np.array([[capshape,capsurface,capcolor,bruises,odor,gillattachment,gillspacing,gillsize,gillcolor,stalkshape,stalkroot,
+                        stalk_surface_above_ring,stalk_surface_below_ring,stalk_color_above_ring,stalk_color_below_ring,veilcolor,
+                        ringnumber,ringtype,spore_print_color,population,habitat]])
+
+        prediction = classifier_model.predict(input1)
+        return str(prediction)
 
 if __name__ == '__main__':
         app.run(debug=True)
